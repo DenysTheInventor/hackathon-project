714de37fa851c7a5004e961931848baadfa908f2
Sergii Bodnar <serg_bod@ukr.net>
2022-06-25 19:16:19 +0300

    paint and delete random figures

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { ContextMenu } from './menu'
 import { BackgroundModule } from './modules/background.module'
 import { SoundModule } from './modules/sound.module'
 import { GeoLocation } from './modules/location.module'
+import { geometricFigurs } from './modules/index1'
+
 
 
 const contextMenu = new ContextMenu('.menu')
@@ -12,6 +14,8 @@ const contextMenu = new ContextMenu('.menu')
 contextMenu.add(new BackgroundModule('backgrounde-module', 'Background'))
 contextMenu.add(new SoundModule('sound-module', 'Play sound'))
 contextMenu.add(new GeoLocation('location-module', 'Geolocation'))
+contextMenu.add(new geometricFigurs('random-figures-module', 'random figure'))
+
 
 // init app
 contextMenu.init()
