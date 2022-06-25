846ebc61f132799d869bc56b095721d294d71d51
Denis Holubchikov <dgolubchicov@gmail.com>
2022-06-25 07:59:26 +0300

    added tips for team

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import './styles.css'
 import { ContextMenu } from './menu'
+// attach modules here
 import { BackgroundModule } from './modules/background.module'
 
+
 const contextMenu = new ContextMenu('.menu')
+
+// add modules to menu here
 contextMenu.add(new BackgroundModule('backgrounde-module', 'Background'))
 
+
+// init app
 contextMenu.init()
 
