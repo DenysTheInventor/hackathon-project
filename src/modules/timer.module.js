c4a3d9890358c42df6fbc0e40bd9c2823874f50a
Anton Nosov <nosov.anton1994@gmail.com>
2022-06-25 15:37:17 +0300

    -

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -7,8 +7,6 @@ export class TimerModule extends Module {
 
     constructor(type, text) {
         super(type, text)
-        this.form = document.createElement('form')
-        this.form.className = 'timer-form'
     }
 
     trigger() {
@@ -16,77 +14,75 @@ export class TimerModule extends Module {
         document.querySelector('.wrapper').append(timerToHTML)
     }
 
-    toHTML(){
-        // const form = document.createElement('form')
-        // form.className = 'timer-form'
+    toHTML() {
+        document.querySelector('.wrapper').insertAdjacentHTML('beforeend',
+        `<div class="timer-container">
+            <form class="timer-form">
+                <label class="timer-form__input-label">
+                    Введите время для таймера
+                    <input class="timer-form__hours-input" name="hours" type="number" max="24" min="0" required="">
+                    <input class="timer-form__hours-input" name="minutes" type="number" max="60" min="0" required="">
+                    <input class="timer-form__hours-input" name="seconds" type="number" max="60" min="0" required="">
+                    <button class="timer-form__submit-button" type="submit">Запустить таймер</button>
+                </label>
+            </form>
+            <button class="timer-form__finish-button">Завершить</button>
+        </div>`
+        )
 
-        const timerLabel = document.createElement('label')
-        timerLabel.className = 'timer-form__input-label'
-        timerLabel.textContent = 'Введите время для таймера'
+        const timerContainer = document.querySelector('.timer-container')
+        const form = document.querySelector('.timer-form')
+        const timerLabel = document.querySelector('.timer-form__input-label')
 
-        const hoursImputLabel = document.createElement('input')
-        hoursImputLabel.className = 'timer-form__hours-input'
-        hoursImputLabel.name = 'hours'
-        hoursImputLabel.type = 'number'
-        hoursImputLabel.max = '24'
-        hoursImputLabel.min = '0'
-        hoursImputLabel.required = ''
-
-        const minutesImputLabel = document.createElement('input')
-        minutesImputLabel.className = 'timer-form__hours-input'
-        minutesImputLabel.name = 'minutes'
-        minutesImputLabel.type = 'number'
-        minutesImputLabel.max = '60'
-        minutesImputLabel.min = '0'
-        minutesImputLabel.required = ''
-
-        const secondsImputLabel = document.createElement('input')
-        secondsImputLabel.className = 'timer-form__hours-input'
-        secondsImputLabel.name = 'seconds'
-        secondsImputLabel.type = 'number'
-        secondsImputLabel.max = '60'
-        secondsImputLabel.min = '0'
-        secondsImputLabel.required = 'number'
-
-        const timerFormButton = document.createElement('button')
-        timerFormButton.className = 'timer-form__submit-button'
-        timerFormButton.type = 'submit'
-        timerFormButton.textContent = 'Запустить таймер'
-
-        timerLabel.append(hoursImputLabel, minutesImputLabel, secondsImputLabel, timerFormButton)
-
-        this.form.append(timerLabel)
-
-        this.form.addEventListener('submit', (event) => {
+        form.addEventListener('submit', (event) => {
             event.preventDefault()
 
             let amountOfHours = Number((event.target.hours.value))
             let amountOfMinutes = Number((event.target.minutes.value))
             let amountOfSeconds = Number((event.target.seconds.value))
 
-            timerLabel.textContent = `Таймер сработает через ${amountOfHours} ч. ${amountOfMinutes} мин. ${amountOfSeconds} сек.`
-            
-            const timer = setInterval(() => {
-                if (amountOfSeconds <= 0 && amountOfMinutes > 0) {
-                    amountOfMinutes = amountOfMinutes - 1
-                    amountOfSeconds = 2
+            timer(amountOfHours, amountOfMinutes, amountOfSeconds, timerContainer)
+        })
+
+        function addZero(number) {
+            if (number <= 9) {
+                number = '0' + number
+            } return number
+        }
+
+        let timerIntervalID
+
+        function timer(hours, minutes, seconds, container) {
+            timerIntervalID = setInterval(() => {
+                if (seconds <= 0 && minutes > 0) {
+                    minutes = minutes - 1
+                    seconds = 60
                 }
-                if (amountOfMinutes <= 0 && amountOfHours > 0) {
-                    amountOfHours = amountOfHours - 1
-                    amountOfMinutes = 5
+                if (minutes <= 0 && hours > 0) {
+                    hours = hours - 1
+                    minutes = 60
                 }
-                if (amountOfSeconds <= 0 && amountOfMinutes <= 0 && amountOfHours <= 0) {
-                    clearInterval(timer);
+                if (seconds <= 0 && minutes <= 0 && hours <= 0) {
                     timerLabel.textContent = 'Таймер завершил свою работу'
                     setTimeout(() => {
-                        this.form.remove()
+                        container.remove()
                     },2000)
                 } else {
-                    timerLabel.textContent = `Таймер сработает через ${amountOfHours} ч. ${amountOfMinutes} мин. ${amountOfSeconds} сек.`;
+                    timerLabel.textContent = `Таймер сработает через ${addZero(hours)} ч. ${addZero(minutes)} мин. ${addZero(seconds)} сек.`;
                 }
-                --amountOfSeconds;
+                --seconds;
             }, 1000)
+        }
+
+        const finishFormButton = document.querySelector('.timer-form__finish-button')
+
+        finishFormButton.addEventListener('click', (event) => {
+            clearInterval(timerIntervalID)
+            timerLabel.textContent = 'Таймер завершил свою работу'
+            setTimeout(() => {
+                timerContainer.remove()
+            },2000)
         })
-        return this.form
+        return timerContainer
     }
 }
